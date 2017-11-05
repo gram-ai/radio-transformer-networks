99dcc3cc5762623bed41f8da7bbda65c5e8a30c6
Kenta Iwasaki <kenta@gram.ai>
2017-11-05 19:20:35 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,67 @@
+# An Introduction to Deep Learning for the Physical Layer
+
+An usable PyTorch implementation of the noisy autoencoder infrastructure in the paper "An Introduction to Deep Learning for the Physical Layer" by [Kenta Iwasaki](https://github.com/iwasaki-kenta) on behalf of Gram.AI.
+
+Overall a fun experiment for constructing a communications system for the physical layer with transmitters/receivers in which the transmitter efficiently encodes a signal in a way such that the receiver can still, with minimal error, decode this encoded signal despite being inflicted with noise in amidst transmission.
+
+The signal dimension for the encoded message is set to be 4, with the compressed signal representation's channel size being 2 (log_2(signal_dim)) to maximize information/bit as a basis to the principles of shannon entropy.
+
+The signal-to-noise ratio simulated in amidst training is 7dbW. That may be changed accordingly to your preferences.
+
+Checks for the bit error rate have been ignored for the decoder, and instead the reconstruction of the input based on categorical cross-entropy is used to validate model generalization and performance.
+
+Training for the model is done using [TorchNet](https://github.com/pytorch/tnt).
+
+## Description
+
+> We present and discuss several novel applications
+of deep learning (DL) for the physical layer. By interpreting
+a communications system as an autoencoder, we develop a
+fundamental new way to think about communications system
+design as an end-to-end reconstruction task that seeks to jointly
+optimize transmitter and receiver components in a single process.
+We show how this idea can be extended to networks of multiple
+transmitters and receivers and present the concept of radio
+transformer networks (RTNs) as a means to incorporate expert
+domain knowledge in the machine learning (ML) model. Lastly,
+we demonstrate the application of convolutional neural networks
+(CNNs) on raw IQ samples for modulation classification which
+achieves competitive accuracy with respect to traditional schemes
+relying on expert features. The paper is concluded with a
+discussion of open challenges and areas for future investigation.
+
+Paper written by Tim O'Shea and Jakob Hoydis. For more information, please check out the paper [here](https://arxiv.org/abs/1702.00832).
+
+## Requirements
+
+* Python 3
+* PyTorch
+* TorchNet
+* TQDM
+
+## Usage
+
+
+**Step 1** Start training.
+
+```console
+$ python3 radio_transformer_networks.py
+```
+
+**Step 2** Call `model.decode_signal(x)` on any noisy data on the transmitter's end.
+
+## Benchmarks
+
+Easily achieves ~98%+ within a span of 100 epochs.
+
+Default PyTorch Adam optimizer hyperparameters were used with no learning rate scheduling. Epochs with batch size of 256 takes half a second on a Razer Blade w/ GTX 1050. 
+
+## TODO
+
+* Signal modulation classification using convolutional neural networks as outlined on the paper.
+
+## Contact/Support
+
+Gram.AI is currently heavily developing a wide number of AI models to be either open-sourced or released for free to the community, hence why we cannot guarantee complete support for this work.
+
+If any issues come up with the usage of this implementation however, or if you would like to contribute in any way, please feel free to send an e-mail to [kenta@gram.ai](kenta@gram.ai) or open a new GitHub issue on this repository.
---
+++ radio_transformer_networks.py
@@ -0,0 +1,134 @@
+import torch
+from torch import nn
+
+
+class RadioTransformerNetwork(nn.Module):
+    def __init__(self, in_channels, compressed_dim):
+        super(RadioTransformerNetwork, self).__init__()
+
+        self.in_channels = in_channels
+
+        self.encoder = nn.Sequential(
+            nn.Linear(in_channels, in_channels),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_channels, compressed_dim),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(compressed_dim, compressed_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(compressed_dim, in_channels)
+        )
+
+    def decode_signal(self, x):
+        return self.decoder(x)
+
+    def forward(self, x):
+        x = self.encoder(x)
+
+        # Normalization
+        x = (self.in_channels ** 2 / x.norm()) * x
+
+        # 7dBW to Er/N0
+        training_signal_noise_ratio = 5.01187
+
+        # bit / channel_use
+        communication_rate = 1
+
+        x += Variable(torch.randn(*x.size()) / ((2 * communication_rate * training_signal_noise_ratio) ** 0.5)).cuda()
+
+        x = self.decoder(x)
+
+        return x
+
+
+if __name__ == "__main__":
+    from tqdm import tqdm
+    from torchnet.engine import Engine
+    from torch.autograd import Variable
+    from torch.optim import Adam
+    import torchnet as tnt
+
+    channel_size = 4
+
+    model = RadioTransformerNetwork(channel_size, compressed_dim=2)
+    model.cuda()
+
+    train_labels = (torch.rand(10000) * channel_size).long()
+    train_data = torch.sparse.torch.eye(channel_size).index_select(dim=0, index=train_labels)
+
+    test_labels = (torch.rand(1500) * channel_size).long()
+    test_data = torch.sparse.torch.eye(channel_size).index_select(dim=0, index=test_labels)
+
+    optimizer = Adam(model.parameters())
+
+    engine = Engine()
+    meter_loss = tnt.meter.AverageValueMeter()
+    meter_accuracy = tnt.meter.ClassErrorMeter(accuracy=True)
+    confusion_meter = tnt.meter.ConfusionMeter(channel_size, normalized=True)
+
+    loss_fn = nn.CrossEntropyLoss()
+
+
+    def get_iterator(mode):
+        data = train_data if mode else test_data
+        labels = train_labels if mode else test_labels
+        tensor_dataset = tnt.dataset.TensorDataset([data, labels])
+
+        return tensor_dataset.parallel(batch_size=256, num_workers=4, shuffle=mode)
+
+
+    def processor(sample):
+        data, labels, training = sample
+
+        data = Variable(data).cuda()
+        labels = Variable(labels).cuda()
+
+        outputs = model(data)
+
+        loss = loss_fn(outputs, labels)
+
+        return loss, outputs
+
+
+    def reset_meters():
+        meter_accuracy.reset()
+        meter_loss.reset()
+        confusion_meter.reset()
+
+
+    def on_sample(state):
+        state['sample'].append(state['train'])
+
+
+    def on_forward(state):
+        meter_accuracy.add(state['output'].data, torch.LongTensor(state['sample'][1]))
+        confusion_meter.add(state['output'].data, torch.LongTensor(state['sample'][1]))
+        meter_loss.add(state['loss'].data[0])
+
+
+    def on_start_epoch(state):
+        reset_meters()
+        state['iterator'] = tqdm(state['iterator'])
+
+
+    def on_end_epoch(state):
+        print('[Epoch %d] Training Loss: %.4f (Accuracy: %.2f%%)' % (
+            state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0]))
+
+        reset_meters()
+
+        engine.test(processor, get_iterator(False))
+
+        print('[Epoch %d] Testing Loss: %.4f (Accuracy: %.2f%%)' % (
+            state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0]))
+
+
+    engine.hooks['on_sample'] = on_sample
+    engine.hooks['on_forward'] = on_forward
+    engine.hooks['on_start_epoch'] = on_start_epoch
+    engine.hooks['on_end_epoch'] = on_end_epoch
+
+    engine.train(processor, get_iterator(True), maxepoch=100, optimizer=optimizer)
+
+
